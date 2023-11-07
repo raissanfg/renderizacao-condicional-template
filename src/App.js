f26688d7723ed6b26b30fbffa4c604cb009fe502
raissanfg <raissanfg@gmail.com>
2023-11-07 13:25:20 -0300

    exercicio-template-feito

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import styled, { createGlobalStyle } from "styled-components";
 import TelaLogin from "./components/TelaLogin/TelaLogin";
 import TelaCadastro from "./components/TelaCadastro/TelaCadastro";
 import TelaPrincipal from "./components/TelaPrincipal/TelaPrincipal.js";
+import { useState } from "react";
+
 
 const GlobalStyled = createGlobalStyle`
   html {
@@ -19,18 +21,70 @@ const MainContainer = styled.main`
 `
 
 function App() {
+  const [telaAtual, setTelaAtual] = useState("telaLogin");
 
+
+  // const telaSelecionada = telaAtual === "telaLogin" ? (
+  //   <TelaLogin setTelaAtual={setTelaAtual} />
+  // ) : (
+  //   <TelaCadastro  setTelaAtual={setTelaAtual}/>
+  // )
+
+ const selecionarTela = () => {
+  switch(telaAtual){
+
+    case "telaLogin" :
+    return <TelaLogin setTelaAtual={setTelaAtual}/> 
+
+    case "telaCadastro" :
+      return <TelaCadastro setTelaAtual={setTelaAtual}/> 
+
+     case "telaPrincipal"  :
+      return <TelaPrincipal setTelaAtual={setTelaAtual}/>
+
+   default: 
+   return <TelaLogin />
+  }
+
+ }
+
+ const telaSelecionada = selecionarTela();
   return (
     <MainContainer >
       <GlobalStyled />
 
-      <TelaLogin />
+      {telaSelecionada}
 
-      <TelaCadastro />
-      
-      <TelaPrincipal />
+     
     </MainContainer>
   );
 }
 
 export default App;
+
+
+
+ // if (telaAtual === "telaLogin") {
+  //   return <TelaLogin />
+  // }
+  // if (telaAtual === "telaCadastro") {
+  //   return <TelaCadastro />
+  // }
+  // if (telaAtual === "telaPrincipal") {
+  //   return <TelaPrincipal />
+  // }
+
+  // switch(telaAtual){
+
+  //   case "telaLogin":
+  //   return <TelaLogin telaAtual={telaAtual}/>
+
+  //   case "telaCadastro":
+  //   return <TelaCadastro/>
+
+  //   case "telaPrincipal":
+  //     return <TelaPrincipal />
+
+  //   default: 
+  //     return <>Pagina nao encontrada</>
+  //   }
--- src/components/TelaCadastro/TelaCadastro.js
+++ src/components/TelaCadastro/TelaCadastro.js
@@ -1,13 +1,13 @@
 import { Form, FormContainer, Input, StyledLabel, SendButton, BackToLoginButton } from "./styled";
 
-function TelaCadastro() {
+function TelaCadastro( {setTelaAtual} ) {
   const cadastrar = () => {
     // validação de cadastro ainda será visto mais pra frente no curso
-    alert("Cadastro realizado com sucesso, mas falta fazer a prática 3 e remover esse alerta")
+    setTelaAtual("telaPrincipal")
   }
 
   const mostrarTelaLogin = () => {
-    alert("Faça a prática 2 e remova esse alerta")
+   setTelaAtual("telaLogin")
   }
 
   return (
